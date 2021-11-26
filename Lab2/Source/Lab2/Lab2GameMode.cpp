21191f57fe1703f936c68de0ef858bdc11632b1a
Lemonaire <297100103@qq.com>
2021-11-26 10:48:39 +0800

    fix bug

--- Lab2/Source/Lab2/Lab2GameMode.cpp
+++ Lab2/Source/Lab2/Lab2GameMode.cpp
@@ -9,7 +9,7 @@
 ALab2GameMode::ALab2GameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
